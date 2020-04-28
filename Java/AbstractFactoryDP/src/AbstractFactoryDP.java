11787dd9e845d68e5de0383f914c054261ae40a3
Wes <wesleyotero@protonmail.com>
2020-04-28 15:23:27 -0400

    Update Test03

--- Java/AbstractFactoryDP/src/AbstractFactoryDP.java
+++ Java/AbstractFactoryDP/src/AbstractFactoryDP.java
@@ -1,6 +1,7 @@
 public class AbstractFactoryDP {
     public static void main(String... args) {
         AbstractFactory abstractFactory = FactoryProducer.getFactory(false);
+        // Adding commenting comment!
         Shape shape = abstractFactory.getShape("Rectangle");
         shape.draw();
     }
